587ef20ca335bb3ec9f4a77eef6664c69918743e
NullPrograms <adoopie100@gmail.com>
2020-12-07 12:40:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+wifis = [line.split(':')[1][1:-1] for line in data if "ALl User Profile" in line]
+
+for wifi in wifis:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', wifi, 'key=clear']).decode('utf-8').split('\n')
+    results = [line.split(':')[1][1:-1] for line in results if "Key Content" in line]
+    try:
+        print(f'Name:{wifi}, Password {results[0]}')
+    except IndexError:
+        print(f'Name:{wifi}, Password {Cannot Be Read}')
+print (wifis)
